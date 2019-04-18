55c323ce735624fa35349d60d13410f704522647
Popeye Lau <popeyelau@gmail.com>
2019-04-18 22:46:06 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# dogeTV for macOS
+
+---
+
+![Demo](./screenshots/1.png)
+![Demo](./screenshots/2.png)
+![Demo](./screenshots/3.png)
+![Demo](./screenshots/4.png)
+![Demo](./screenshots/5.png)
+![Demo](./screenshots/6.png)
+![Demo](./screenshots/7.png)
+![Demo](./screenshots/8.png)
--- dogeTV/Components/SearchBarView.swift
+++ dogeTV/Components/SearchBarView.swift
@@ -51,7 +51,7 @@ class SearchBarView: NSView, LoadableNib {
         wantsLayer = true
         layer?.cornerRadius = 15
         layer?.masksToBounds = true
-        layer?.backgroundColor = NSColor(srgbRed:0.23, green:0.24, blue:0.25, alpha:1.00).cgColor
+        layer?.backgroundColor = NSColor(red:0.12, green:0.13, blue:0.13, alpha:1.00).cgColor
         textField.focusRingType = .none
         textField.backgroundColor = .clear
         let attr = NSAttributedString(string: "搜索电影/演员/导演/视频云解析", attributes: [.foregroundColor: NSColor.lightGray, .font: textField.font!])
